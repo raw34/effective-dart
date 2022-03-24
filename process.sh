287a8eae32c7658baa13f7e6a072c7665dcba363
raw34 <raw34@sina.com>
2022-03-24 12:18:11 +0800

    update

--- process.sh
+++ process.sh
@@ -8,3 +8,12 @@ grep '^    - \[' usage.md | wc -l &&
 grep '^- \[' usage.md | wc -l &&
 grep '^    - \[' design.md | wc -l &&
 grep '^- \[' design.md | wc -l &&
+
+grep '^    - \[' style.md | >> style.out &&
+grep '^- \[' style.md | >> style.out &&
+grep '^    - \[' documentation.md | >> documentation.out  &&
+grep '^- \[' documentation.md | >> documentation.out &&
+grep '^    - \[' usage.md | >> usage.out &&
+grep '^- \[' usage.md | >> usage.out &&
+grep '^    - \[' design.md | >> design.out &&
+grep '^- \[' design.md | >> design.out &&
